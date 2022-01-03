684fd01f99dccec9effb0494cb959107cebfada5
IrvinSanchezQ <irvin.iazq@gmail.com>
2022-01-02 21:20:41 -0700

    modificacion de puerto
    en esta actuliazacion se realizo un cambio en el puerto asignado para realizar pruebas con heroku

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const connectDB = require('./database/connection');
 const app = express();
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
+const Resgistro = require ('./models/ClientModel');
+const { body, validationResult } = require('express-validator');
 
 //conexion este archivpo sirve de servidor de rutas o algo asi 
 //var mongoose = require('mongoose')
@@ -31,22 +33,104 @@ const PORT = process.env.PORT || 8080
 // })  .then(db => console.log('db is connected'))
 //     .catch(err => console.log(err));
 
-// mongo_url = mongodb+srv://ADMIN:ADMIN123EIF@cluster0.8iasn.mongodb.net/BanCoppelDB?retryWrites=true&w=majority
 //mongodb connection
 connectDB();
 //Settings
-//app.set('port', process.env.PORT || 3000);
 
 //middlewares
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(morgan('dev'));
 //app.use(express.urlencoded({extended: true}));
-//diosito help
+
 app.use(bodyParser.urlencoded({extended: false}));
+
+//contenido estatico mostrado y solicitado por node
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => {
+
+//aqui creamos la conexion con el frond end o formularios
+//min 17 https://www.youtube.com/watch?v=GlvpYCEEcVQ
+app.post('/registroUsuario', (req, res) => {
+    const {Nombre,APaterno,AMaterno,Email,password,IdCliente, Direccion,Telefono,ciudad,Estado,CodigoPostal} = req.body;
+    const registro = new Resgistro({Nombre,APaterno,AMaterno,Email,password,IdCliente, Direccion,Telefono,ciudad,Estado,CodigoPostal});
+    registro.save(err =>{
+        if(err){
+            res.status(500).send('error al resgistrar al usuario');
+        }else{
+            res.status(200).send('usuario registrado');
+        }
+    });
 });
+app.post('/iniciosesion', (req, res) => {
+    const {IdCuenta,password} = req.body;
+    
+    Resgistro.findOne({IdCuenta}, (err, registro) =>{
+        if(err){
+            res.status(500).send('erro al validar la cuenta');
+        }else if(!registro){
+            res.status(500).send('el usuario no existe');
+        }else{
+            registro.isCorrectPassword(password,(err, result) =>{
+                if(err){
+                    res.status(500).send('Error al autenticar');
+                }else if(result){
+                    res.status(200).send('usuario autenticado correctamente');
+                }else{
+                    res.status(500).send('usuario y/o contraseña incorrectos')
+                }
+            });
+        }
+    });
+});
+//  app.post('/registroUsuarioValidado',[
+//      body('Nombre', 'ingresa un nombre valido')
+//         .exists()
+//         .isLength({min:3}),
+//      body('APaterno','ingresa un apellido valido 1')
+//         .exists()
+//         .isLength({min:3}),
+//      body('AMaterno','ingresa un apellido valido 2')
+//         .exists()
+//         .isLength({min:3}),
+//      body('Email', 'ingresa un email valido')
+//          .exists()
+//          .isEmail(),
+//      body('password', 'ingesa una contraseña valida')
+//          .exists()
+//          .isStrongPassword(),
+//      body('Direccion', 'ingresa una direccion valida')
+//          .exists()
+//          .isString(),
+
+//      body('Telefono', 'ingresa un telefono valido')
+//         .exists()
+//         .isMobilePhone(),
+//      body('ciudad', 'ingrese el nombre de la ciudad donde habita')
+//         .exists()
+//         .isString(),
+//      body('Estado', 'seleccione un estado')
+//          .exists()
+//          .isString(),
+//      body('CodigoPostal', 'seleccione un numero postal valido')
+//         .exists()
+//         .isLength({max:5})
+//         .isLength({min:5})
+//  ],(req,res)=>{
+//     const errores = validationResult(req)
+//     if(!errores.isEmpty()){
+//         Console.log(req.body)
+//         const valores = req.body
+//         const validaciones = errores.array()
+//         //aqui se añade la renderizacion de la paguina con errores
+//         //res.render('index', {validaciones:validaciones, valores:valores})
+//     }else{
+//         res.send('¡¡validacion exitosa!!')
+//     }
+//  });
+
+/*aqui estaba bien broder
+app.get('/', (req, res) => {
+});*/
 //ROUTES
 //app.use('registroUsuario.html',require('src/public/registroUsuario.html'));
 app.use(require('./routes/index'));
--- src/models/ClientModel.js
+++ src/models/ClientModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const saldRounds =10;
  //const Schema = mongoose.Schema;
 
 const  clienteSchema = new mongoose.Schema ({
@@ -6,11 +9,42 @@ const  clienteSchema = new mongoose.Schema ({
     APaterno:String,
     AMaterno:String,
     Email: String,
+    password: String,
     IdCliente: String,
+    Direccion: String,
+    Telefono: String,
+    ciudad: String,
+    Estado: String,
+    CodigoPostal: String,
     FechaNacimiento: String,
-    Nip: String
-    //,IdCuenta
+    Nip: String,
+    IdCuenta: String 
 });
+clienteSchema.pre('save', function (next) {
+    if(this.isNew || this.isModified('password')){
+        const document = this;
+        bcrypt.hash(document.password,saldRounds,(err,hashedPassword) =>{
+            if(err){
+                next(err);
+            }else{
+                document.password = hashedPassword;
+                next();
+            }
+        });
+    }else{
+        next();
+    }
+    
+});
+clienteSchema.methods.isCorrectPassword = function(password, callback){
+    bcrypt.compare(password,this.password, function(err, same) {
+        if(err){
+            callback(err);
+        }else{
+            callback(err, same);
+         }
+    });
+}
 
 module.exports = mongoose.model('Clientdb', clienteSchema);
    
